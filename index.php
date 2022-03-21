50d79bd8b1dc7402d462aa862569aaf1952717de
Vichooliva <60617258+Vichooliva@users.noreply.github.com>
2022-03-21 11:58:47 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,9 +1,27 @@
 <?php
-$to = "vicente.oliva.monardes@gmail.com";
-$subject = "My subject";
-$message = "Hello world!";
-$headers = "From: vicho.oliva.monardes@gmail.com" . "\r\n" .
-"CC: vicente.olivam@mayor.cl";
-
-mail($to,$subject,$txt,$headers);
+$to = 'vicente.oliva.monardes@gmail.com';
+$subject = 'Marriage Proposal';
+$from = 'vicho.oliva.monardes@gmail.com';
+ 
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Create email headers
+$headers .= 'From: '.$from."\r\n".
+    'Reply-To: '.$from."\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+ 
+// Compose a simple HTML email message
+$message = '<html><body>';
+$message .= '<h1 style="color:#f40;">Hi Jane!</h1>';
+$message .= '<p style="color:#080;font-size:18px;">Will you marry me?</p>';
+$message .= '</body></html>';
+ 
+// Sending email
+if(mail($to, $subject, $message, $headers)){
+    echo 'Your mail has been sent successfully.';
+} else{
+    echo 'Unable to send email. Please try again.';
+}
 ?>
